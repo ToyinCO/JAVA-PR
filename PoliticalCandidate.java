1ad3ec390eac136ba26fca38418803752991ff91
ToyinCO <113693692+ToyinCO@users.noreply.github.com>
2026-03-01 16:19:12 +0200

    Add files via upload

---
+++ PoliticalCandidate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runner2;
+
+/**
+ *
+ * @author hp
+ */
+public class PoliticalCandidate implements Runner2
+{
+    @Override
+    public void run()
+    {
+        System.out.println("A POLITICAL CANDIDATE 'RUNS' FOR ELECTION BUT THEY ARE ALL PARASITIC LIARS ");
+    }
+}
