c1ef567bce6b7c2836be42abfbe14c6c6ac31be2
ToyinCO <113693692+ToyinCO@users.noreply.github.com>
2026-03-01 16:19:57 +0200

    Add files via upload

---
+++ Runner2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runner2;
+
+/**
+ *
+ * @author hp
+ */
+public interface Runner2
+{
+public abstract void run();
+        
+}
